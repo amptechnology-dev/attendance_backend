ead74a06dcdb486d49d19824dfee5d5947437fd9
Amp Technology <devs.amptechnology@gmail.com>
2026-06-26 19:36:11 +0530

    Solve MongoDB Issue

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
+import dns from 'dns';
 import logger from './logger.js';
 
-// Connect to MongoDB
+// Force Node.js to use Google DNS
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
